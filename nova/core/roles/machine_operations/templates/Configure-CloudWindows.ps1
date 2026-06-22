2c10dcaf1ee7afd1a7cb37e2b2f26bc32cae76d4
Allar Viik <allar@clarifiedsecurity.com>
2026-06-22 10:29:42 +0300

    Added Google Cloud Platform support to `machine_operations` & `connection` role

--- nova/core/roles/machine_operations/templates/Configure-CloudWindows.ps1
+++ nova/core/roles/machine_operations/templates/Configure-CloudWindows.ps1
@@ -1,70 +1,77 @@
 {% if infra_env == "aws" %}
 <powershell>
 {% endif %}
-$ErrorActionPreference = 'Stop'
+{% if infra_env == "google" %}
+windows-startup-script-ps1: |
+{% endif %}
+    $ErrorActionPreference = 'Stop'
 
-$LogPath = "C:\Windows\Temp\install.log"
-$RemoteConnectivityHost = "google.com"
-$OS = Get-WmiObject -Class Win32_OperatingSystem
-$version = [version]$OS.Version
-$SSHDownloadLink = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/10.0.0.0p2-Preview/OpenSSH-Win64-v10.0.0.0.msi"
+    $LogPath = "C:\Windows\Temp\install.log"
+    $RemoteConnectivityHost = "google.com"
+    $OS = Get-WmiObject -Class Win32_OperatingSystem
+    $version = [version]$OS.Version
+    $SSHDownloadLink = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/10.0.0.0p2-Preview/OpenSSH-Win64-v10.0.0.0.msi"
 
-#--------------------End of variables, Start of Script--------------------#
+    #--------------------End of variables, Start of Script--------------------#
 
-Start-Transcript -Path $LogPath
+    Start-Transcript -Path $LogPath
+
+{% if infra_env in ["aws", "google"] %}
+    Write-Host "Setting Administrator password"
+    $Password = "{{ template_password }}"
+    $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+    Set-LocalUser -Name "Administrator" -Password $SecurePassword
+    Enable-LocalUser -Name "Administrator"
+{% endif %}
 
 {% if infra_env == "aws" %}
-Write-Host "Setting Administrator password"
-$Password = "{{ template_password }}"
-$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
-Set-LocalUser -Name "Administrator" -Password $SecurePassword
-
-Write-Host "Setting temporary SSH key for Administrator user"
-$TempSSHKey = "{{ slurped_aws_temp_ssh_key.content | b64decode }}"
-Set-Content -Path "C:\ProgramData\ssh\administrators_authorized_keys" -Value $TempSSHKey -Force
+    Write-Host "Setting temporary SSH key for Administrator user"
+    $TempSSHKey = "{{ slurped_aws_temp_ssh_key.content | b64decode }}"
+    Set-Content -Path "C:\ProgramData\ssh\administrators_authorized_keys" -Value $TempSSHKey -Force
 {% endif %}
 
 {% if infra_env == "azure" %}
-Rename-LocalUser -Name "azureadmin" -NewName "Administrator"
+    Rename-LocalUser -Name "azureadmin" -NewName "Administrator"
+    Enable-LocalUser -Name "Administrator"
 {% endif %}
 
-Write-Host "Checking that $RemoteConnectivityHost is reachable"
-while($true) {
-    $ping = Test-Connection -ComputerName $RemoteConnectivityHost -Count 1 -Quiet
-    if($ping) {
-        Write-Host "$RemoteConnectivityHost is reachable."
-        break
-    } else {
-        Write-Host "Cannot reach $RemoteConnectivityHost. Retrying..."
-        Start-Sleep -Seconds 3
+    Write-Host "Checking that $RemoteConnectivityHost is reachable"
+    while($true) {
+        $ping = Test-Connection -ComputerName $RemoteConnectivityHost -Count 1 -Quiet
+        if($ping) {
+            Write-Host "$RemoteConnectivityHost is reachable."
+            break
+        } else {
+            Write-Host "Cannot reach $RemoteConnectivityHost. Retrying..."
+            Start-Sleep -Seconds 3
+        }
     }
-}
 
-Write-Host "Setting PowerShell as default SSH shell"
-reg add HKLM\Software\OpenSSH /v DefaultShell /t REG_SZ /d C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe /f
+    Write-Host "Setting PowerShell as default SSH shell"
+    reg add HKLM\Software\OpenSSH /v DefaultShell /t REG_SZ /d C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe /f
 
-if ($version.Major -ge 10) {
+    if ($version.Major -ge 10) {
 
-    Write-Host "Removing native OpenSSH Client"
-    Remove-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+        Write-Host "Removing native OpenSSH Client"
+        Remove-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 
-}
+    }
 
-Write-Host "Downloading and installing latest OpenSSH"
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-WebRequest -Uri $SSHDownloadLink -OutFile "C:\Windows\Temp\OpenSSH.msi"
-msiexec /i "C:\Windows\Temp\OpenSSH.msi" /qn
+    Write-Host "Downloading and installing latest OpenSSH"
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-WebRequest -Uri $SSHDownloadLink -OutFile "C:\Windows\Temp\OpenSSH.msi"
+    msiexec /i "C:\Windows\Temp\OpenSSH.msi" /qn
 
-while(-not (Get-NetFirewallRule | Where-object DisplayName -like "*OpenSSH*")) {
-    Start-Sleep -Seconds 1
-    Write-Host "Waiting until SSH firewall rule is created"
-}
+    while(-not (Get-NetFirewallRule | Where-object DisplayName -like "*OpenSSH*")) {
+        Start-Sleep -Seconds 1
+        Write-Host "Waiting until SSH firewall rule is created"
+    }
 
-Write-Host "Allowing OpenSSH through firewall with all profiles"
-Get-NetFirewallRule | Where-object DisplayName -like "*OpenSSH*" | Set-NetFirewallRule -Profile Any
+    Write-Host "Allowing OpenSSH through firewall with all profiles"
+    Get-NetFirewallRule | Where-object DisplayName -like "*OpenSSH*" | Set-NetFirewallRule -Profile Any
 
-Remove-item C:\Windows\Temp\OpenSSH.msi -Force
+    Remove-item C:\Windows\Temp\OpenSSH.msi -Force
 
-Stop-Transcript
+    Stop-Transcript
 {% if infra_env == "aws" %}
 </powershell>
 {% endif %}
